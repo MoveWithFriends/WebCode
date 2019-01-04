00f61a2b172e0606c537faaeb74bcbd390f748f8
theomen3 <33961419+theomen3@users.noreply.github.com>
2019-01-04 22:42:09 +0100

    Update install.php

--- install.php
+++ install.php
@@ -28,7 +28,7 @@ $db->query( "CREATE TABLE IF NOT EXISTS users (
         name VARCHAR(30),
         email VARCHAR(255),
         phonenumber VARCHAR(30),
-        birthdate VARCHAR(30),
+        birthdate DATE,
         gender VARCHAR(30),
         preferredgender VARCHAR(30)
     )");
@@ -49,53 +49,53 @@ $db->query( "CREATE TABLE IF NOT EXISTS password_reset (
         expires BIGINT(20)
     )");
 
-// $db->query( "CREATE TABLE IF NOT EXISTS Interest (
-//   CustomerIDinterest  INT(10),
-//   Boksen      BOOLEAN,
-//   Fitness     BOOLEAN,
-//   Hardlopen   BOOLEAN,
-//   Tennis      BOOLEAN,
-//   Squash      BOOLEAN,
-//   Wandelen    BOOLEAN,
-//   Wielrennen  BOOLEAN,
-//   Zwemmen     BOOLEAN,
+ $db->query( "CREATE TABLE IF NOT EXISTS interest (
+   CustomerIDinterest  INT(10),
+   Boksen      BOOLEAN,
+   Fitness     BOOLEAN,
+   Hardlopen   BOOLEAN,
+   Tennis      BOOLEAN,
+   Squash      BOOLEAN,
+   Wandelen    BOOLEAN,
+   Wielrennen  BOOLEAN,
+   Zwemmen     BOOLEAN,
   
-//   CONSTRAINT
-//     PRIMARY KEY InterestPK (CustomerIDinterest, Boksen),
+   CONSTRAINT
+     PRIMARY KEY InterestPK (CustomerIDinterest, Boksen),
 
-//   CONSTRAINT
-//     FOREIGN KEY InterestFK (CustomerIDinterest)
-//     REFERENCES Customer (CustomerID)
-//     )");
+   CONSTRAINT
+     FOREIGN KEY InterestFK (CustomerIDinterest)
+     REFERENCES users (CustomerID)
+     )");
 
-// $db->query( "CREATE TABLE IF NOT EXISTS Timeslot  (
-//   CustomerIDtimeslot   INT(10),
-//   Timeslot1     BOOLEAN,
-//   Timeslot2     BOOLEAN,
-//   Timeslot3     BOOLEAN,
-//   Timeslot4     BOOLEAN,
-//   Timeslot5     BOOLEAN,
-//   Timeslot6     BOOLEAN,
-//   Timeslot7     BOOLEAN,
-//   Timeslot8     BOOLEAN,
-//   Timeslot9     BOOLEAN,
-//   Timeslot10    BOOLEAN,
-//   Timeslot11    BOOLEAN,
-//   Timeslot12    BOOLEAN,
-//   Timeslot13    BOOLEAN,
-//   Timeslot14    BOOLEAN,
-//   Timeslot15    BOOLEAN,
-//   Timeslot16    BOOLEAN,
-//   Timeslot17    BOOLEAN,
-//   Timeslot18    BOOLEAN,
-//   Timeslot19    BOOLEAN,
-//   Timeslot20    BOOLEAN,
-//   Timeslot21    BOOLEAN,
+ $db->query( "CREATE TABLE IF NOT EXISTS timeslot  (
+   CustomerIDtimeslot   INT(10),
+   Timeslot1     BOOLEAN,
+   Timeslot2     BOOLEAN,
+   Timeslot3     BOOLEAN,
+   Timeslot4     BOOLEAN,
+   Timeslot5     BOOLEAN,
+   Timeslot6     BOOLEAN,
+   Timeslot7     BOOLEAN,
+   Timeslot8     BOOLEAN,
+   Timeslot9     BOOLEAN,
+   Timeslot10    BOOLEAN,
+   Timeslot11    BOOLEAN,
+   Timeslot12    BOOLEAN,
+   Timeslot13    BOOLEAN,
+   Timeslot14    BOOLEAN,
+   Timeslot15    BOOLEAN,
+   Timeslot16    BOOLEAN,
+   Timeslot17    BOOLEAN,
+   Timeslot18    BOOLEAN,
+   Timeslot19    BOOLEAN,
+   Timeslot20    BOOLEAN,
+   Timeslot21    BOOLEAN,
   
-// CONSTRAINT
-//     PRIMARY KEY TimeslotPK (CustomerIDtimeslot, Timeslot1),
+ CONSTRAINT
+     PRIMARY KEY TimeslotPK (CustomerIDtimeslot, Timeslot1),
 
-//   CONSTRAINT
-//     FOREIGN KEY TimeslotFK (CustomerIDtimeslot)
-//     REFERENCES Customer (CustomerID)
-//     )");
+   CONSTRAINT
+     FOREIGN KEY TimeslotFK (CustomerIDtimeslot)
+     REFERENCES users (CustomerID)
+    )");
